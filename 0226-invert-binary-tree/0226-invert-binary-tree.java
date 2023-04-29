fec03109d1e2a308050c18a71f6f78fcb285c69f
HyunsooZo <126321038+HyunsooZo@users.noreply.github.com>
2023-04-30 00:25:34 +0900

    Time: 0 ms (100.00%), Space: 40.4 MB (27.08%) - LeetHub

--- 0226-invert-binary-tree/0226-invert-binary-tree.java
+++ 0226-invert-binary-tree/0226-invert-binary-tree.java
@@ -15,14 +15,14 @@
  */
 class Solution {
     public TreeNode invertTree(TreeNode root) {
-        if (root == null) return null;
-
+        if(root == null) return null;
+        
         TreeNode left = invertTree(root.left);
         TreeNode right = invertTree(root.right);
-
+        
         root.left = right;
         root.right = left;
-
+        
         return root;
     }
 }
