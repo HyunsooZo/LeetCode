c843b5cc0c51b80376d26f118b154e1d4c5f717e
HyunsooZo <126321038+HyunsooZo@users.noreply.github.com>
2023-05-01 01:28:30 +0900

    Time: 1 ms (99.98%), Space: 43.9 MB (53.93%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.java
+++ 0283-move-zeroes/0283-move-zeroes.java
@@ -3,15 +3,12 @@ class Solution {
         int i = 0 , j = 0;
         while(j<nums.length){
             if(nums[j]!=0){
-                swap(nums,i,j);
+                int temp = nums[i];
+                nums[i] = nums[j];
+                nums[j] = temp;
                 i++;
             }
             j++;
         }
-    }
-    public void swap(int[] nums ,int i ,int j ){
-        int temp = nums[i];
-        nums[i] = nums[j];
-        nums[j] = temp;
     }
 }
