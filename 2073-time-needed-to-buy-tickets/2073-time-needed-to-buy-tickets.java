d90b81e954c754e2734e93797842f4f124c6cc7b
HyunsooZo <126321038+HyunsooZo@users.noreply.github.com>
2023-05-03 02:31:48 +0900

    Time: 1 ms (65.72%), Space: 40.7 MB (30.81%) - LeetHub

--- 2073-time-needed-to-buy-tickets/2073-time-needed-to-buy-tickets.java
+++ 2073-time-needed-to-buy-tickets/2073-time-needed-to-buy-tickets.java
@@ -1,15 +1,15 @@
 class Solution {
     public int timeRequiredToBuy(int[] tickets, int k) {
-        int count = 0;
+        int time = 0;
         while (tickets[k] != 0) {
             for (int i = 0; i < tickets.length; i++) {
                 if (tickets[i] > 0) {
-                    count++;
+                    time++;
                     tickets[i]--;
                 }
-                if (i == k && tickets[k] == 0) break;
+                if (tickets[k] == 0) break;
             }
         }
-        return count;
+        return time;
     }
 }
