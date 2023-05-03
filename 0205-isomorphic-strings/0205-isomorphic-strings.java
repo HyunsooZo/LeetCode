e974d1a0f97f3f46597e0484b1cdf6df7732bc72
HyunsooZo <126321038+HyunsooZo@users.noreply.github.com>
2023-05-04 02:51:47 +0900

    Time: 5 ms (95.67%), Space: 42.6 MB (50.39%) - LeetHub

--- 0205-isomorphic-strings/0205-isomorphic-strings.java
+++ 0205-isomorphic-strings/0205-isomorphic-strings.java
@@ -3,16 +3,14 @@ class Solution {
         if (s.length() != t.length()) return false;
         int[] sArray = new int[256],tArray = new int[256];
         for (int i = 0; i < s.length(); i++) {
-            char sChar = s.charAt(i);
-            char tChar = t.charAt(i);
+            char sChar = s.charAt(i), tChar = t.charAt(i);           
             sArray[sChar] = i + 1;
             tArray[tChar] = i + 1;
         }
-                for (int i = 0; i < s.length(); i++) {
-                    char sChar = s.charAt(i);
-            char tChar = t.charAt(i);
-                    if (sArray[sChar] != tArray[tChar]) return false;
-                }
+        for (int i = 0; i < s.length(); i++) {
+            char sChar = s.charAt(i) , tChar = t.charAt(i);
+            if (sArray[sChar] != tArray[tChar]) return false;
+        }
         return true;
     }
 }
