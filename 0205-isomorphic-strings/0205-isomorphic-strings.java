96e80612bc7a2cd1b2b5d9549c6e8d528f2438fd
HyunsooZo <126321038+HyunsooZo@users.noreply.github.com>
2023-05-04 02:50:57 +0900

    Time: 5 ms (95.67%), Space: 42.6 MB (37.82%) - LeetHub

--- 0205-isomorphic-strings/0205-isomorphic-strings.java
+++ 0205-isomorphic-strings/0205-isomorphic-strings.java
@@ -5,10 +5,14 @@ class Solution {
         for (int i = 0; i < s.length(); i++) {
             char sChar = s.charAt(i);
             char tChar = t.charAt(i);
-            if (sArray[sChar] != tArray[tChar]) return false;
             sArray[sChar] = i + 1;
             tArray[tChar] = i + 1;
         }
+                for (int i = 0; i < s.length(); i++) {
+                    char sChar = s.charAt(i);
+            char tChar = t.charAt(i);
+                    if (sArray[sChar] != tArray[tChar]) return false;
+                }
         return true;
     }
 }
