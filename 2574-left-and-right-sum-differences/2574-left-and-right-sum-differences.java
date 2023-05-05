34c712ce2684fc0b96a104f42ebffea576350cf7
HyunsooZo <126321038+HyunsooZo@users.noreply.github.com>
2023-05-05 15:34:08 +0900

    Time: 2 ms (62.67%), Space: 43.6 MB (10.97%) - LeetHub

--- 2574-left-and-right-sum-differences/2574-left-and-right-sum-differences.java
+++ 2574-left-and-right-sum-differences/2574-left-and-right-sum-differences.java
@@ -1,13 +1,14 @@
 class Solution {
     public int[] leftRigthDifference(int[] nums) {
         int[] lSum = new int[nums.length], rSum = new int[nums.length], answer = new int[nums.length];
-        for(int i = 1 ; i < nums.length ; i++){
-            if(i==1)lSum[i] = nums[i-1];
-            else lSum[i] = nums[i-1]+lSum[i-1];
-        }
+       
         for(int i= nums.length-2 ; i>=0 ; i--){
             if(i==nums.length-2) rSum[i]= nums[i+1];
             else rSum[i]= nums[i+1]+rSum[i+1];
+        }
+         for(int i = 1 ; i < nums.length ; i++){
+            if(i==1)lSum[i] = nums[i-1];
+            else lSum[i] = nums[i-1]+lSum[i-1];
         }
         for(int i= 0 ; i< answer.length ; i++){
             answer[i] = Math.abs(lSum[i]-rSum[i]);
