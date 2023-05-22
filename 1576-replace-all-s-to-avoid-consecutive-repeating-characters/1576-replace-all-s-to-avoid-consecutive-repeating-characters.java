495c08a1c2c9c1027706aa4491b0589fee86a7d8
HyunsooZo <126321038+HyunsooZo@users.noreply.github.com>
2023-05-23 01:38:05 +0900

    Time: 1 ms (100.00%), Space: 42.1 MB (94.76%) - LeetHub

--- 1576-replace-all-s-to-avoid-consecutive-repeating-characters/1576-replace-all-s-to-avoid-consecutive-repeating-characters.java
+++ 1576-replace-all-s-to-avoid-consecutive-repeating-characters/1576-replace-all-s-to-avoid-consecutive-repeating-characters.java
@@ -1,15 +1,13 @@
-import java.util.Random;
-
 public class Solution {
     public String modifyString(String s) {
         char[] chars = s.toCharArray();
-        Random random = new Random();
+        char[] alphabet = "abcdefghijklmnopqrstuvwxyz".toCharArray();
 
         for (int i = 0; i < chars.length; i++) {
             if (chars[i] == '?') {
                 char prev = (i == 0) ? '?' : chars[i - 1];
                 char next = (i == chars.length - 1) ? '?' : chars[i + 1];
-                char replacement = getRandomChar(prev, next, random);
+                char replacement = getRandomChar(prev, next, alphabet);
                 chars[i] = replacement;
             }
         }
@@ -17,12 +15,12 @@ public class Solution {
         return new String(chars);
     }
 
-    private char getRandomChar(char prev, char next, Random random) {
-        char randomChar;
-        do {
-            randomChar = (char) (random.nextInt(26) + 'a');
-        } while (randomChar == prev || randomChar == next);
-
-        return randomChar;
+    private char getRandomChar(char prev, char next, char[] alphabet) {
+        for (char c : alphabet) {
+            if (c != prev && c != next) {
+                return c;
+            }
+        }
+        return '?'; 
     }
 }
