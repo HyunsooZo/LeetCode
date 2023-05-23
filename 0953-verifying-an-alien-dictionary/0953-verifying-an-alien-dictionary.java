32dfb3f4cd4efddacf22de02ebd532852169b287
HyunsooZo <126321038+HyunsooZo@users.noreply.github.com>
2023-05-24 00:41:25 +0900

    Time: 0 ms (100.00%), Space: 41 MB (86.99%) - LeetHub

--- 0953-verifying-an-alien-dictionary/0953-verifying-an-alien-dictionary.java
+++ 0953-verifying-an-alien-dictionary/0953-verifying-an-alien-dictionary.java
@@ -6,37 +6,24 @@ class Solution {
         for (int i = 0; i < order.length(); i++) {
             charOrder[order.charAt(i) - 'a'] = i;
         }
-
         for (int i = 0; i < words.length - 1; i++) {
             if (!compareWords(words[i], words[i + 1], charOrder)) {
                 return false;
             }
         }
-
         return true;
     }
 
     private boolean compareWords(String word1, String word2, int[] charOrder) {
-        int len1 = word1.length();
-        int len2 = word2.length();
+        int len1 = word1.length(), len2 = word2.length();
         int minLength = Math.min(len1, len2);
-
         for (int i = 0; i < minLength; i++) {
-            char ch1 = word1.charAt(i);
-            char ch2 = word2.charAt(i);
-
+            char ch1 = word1.charAt(i), ch2 = word2.charAt(i);
             if (ch1 != ch2) {
-                int order1 = charOrder[ch1 - 'a'];
-                int order2 = charOrder[ch2 - 'a'];
-
-                if (order1 > order2) {
-                    return false;
-                } else {
-                    return true;
-                }
+                int order1 = charOrder[ch1 - 'a'], order2 = charOrder[ch2 - 'a'];
+                return order1<order2;
             }
         }
-
         return len1 <= len2;
     }
 }
