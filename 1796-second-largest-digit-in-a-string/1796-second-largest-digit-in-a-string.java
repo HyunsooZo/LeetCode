0273dda7d7546553dca51d3c2d484f7b0d1b8293
HyunsooZo <126321038+HyunsooZo@users.noreply.github.com>
2023-05-24 22:41:22 +0900

    Time: 2 ms (84.54%), Space: 41.4 MB (63.98%) - LeetHub

--- 1796-second-largest-digit-in-a-string/1796-second-largest-digit-in-a-string.java
+++ 1796-second-largest-digit-in-a-string/1796-second-largest-digit-in-a-string.java
@@ -1,15 +1,16 @@
 class Solution {
     public int secondHighest(String s) {
-        int max = 0,second = -1;
-        for(char c : s.toCharArray()){
-            if(!Character.isDigit(c)) continue;
-            int i = Character.getNumericValue(c);
-            max = Math.max(max,i);
-        }
-        for(char c : s.toCharArray()){
-            int i = Character.getNumericValue(c);
-            if(!Character.isDigit(c) || max ==i) continue;
-            second = Math.max(second,i);
+        int max = -1, second = -1;
+        for (char c : s.toCharArray()) {
+            if (Character.isDigit(c)) {
+                int num = c - '0';
+                if (num > max) {
+                    second = max;
+                    max = num;
+                } else if (num < max && num > second) {
+                    second = num;
+                }
+            }
         }
         return second;
     }
