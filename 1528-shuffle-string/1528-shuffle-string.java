878087cff29f7dcd853072900111f2742cf414ef
HyunsooZo <126321038+HyunsooZo@users.noreply.github.com>
2023-05-24 23:05:28 +0900

    Time: 1 ms (100.00%), Space: 43.7 MB (5.20%) - LeetHub

--- 1528-shuffle-string/1528-shuffle-string.java
+++ 1528-shuffle-string/1528-shuffle-string.java
@@ -2,12 +2,8 @@ class Solution {
     public String restoreString(String s, int[] indices) {
         StringBuilder sb = new StringBuilder();
         char[] temp = new char[s.length()];
-        for(int i = 0 ; i < s.length() ; i++){
-            temp[indices[i]] = s.charAt(i);
-        }
-        for(char c: temp){
-            sb.append(c);
-        }
+        for(int i = 0 ; i < s.length() ; i++) temp[indices[i]] = s.charAt(i);
+        for(char c: temp) sb.append(c);
         return sb.toString();
     }
 }
