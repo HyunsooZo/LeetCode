d4ab7c8ea7973405ac32d276e6b5d5984208539b
HyunsooZo <126321038+HyunsooZo@users.noreply.github.com>
2023-05-29 15:29:25 +0900

    Time: 2 ms (99.75%), Space: 43.7 MB (15.27%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -4,20 +4,17 @@ class Solution {
     public boolean isAnagram(String s, String t) {
         int slen =  s.length(), tlen =  t.length();
         if (slen != tlen) return false;
-
-        Map<Character, Integer> charCount = new HashMap<>();
-
-        for (int i = 0; i < slen; i++) {
-            char c = s.charAt(i);
-            charCount.put(c, charCount.getOrDefault(c, 0) + 1);
+        int[] alphabet = new int[26];
+        for(char c: s.toCharArray()){
+            alphabet[c-'a']++;
         }
-
-        for (int i = 0; i < tlen; i++) {
-            char c = t.charAt(i);
-            if (!charCount.containsKey(c) || charCount.get(c) == 0) return false;
-            charCount.put(c, charCount.get(c) - 1);
+        for(char c: t.toCharArray()){
+            if(alphabet[c-'a']==0) return false;
+            alphabet[c-'a']--;
         }
-
+        for(int i : alphabet){
+            if (i!=0) return false;
+        }
         return true;
     }
 }
