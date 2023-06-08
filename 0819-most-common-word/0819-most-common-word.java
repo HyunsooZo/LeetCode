258f78ce1c3c2469c3666994ca05774f6e208671
HyunsooZo <126321038+HyunsooZo@users.noreply.github.com>
2023-06-09 08:05:56 +0900

    Time: 14 ms (64.77%), Space: 42.3 MB (85.44%) - LeetHub

---
+++ 0819-most-common-word/0819-most-common-word.java
@@ -0,0 +1,40 @@
+import java.util.*;
+
+class Solution {
+    private class freq {
+        int num = 0;
+        String str;
+
+        freq(int num, String str) {
+            this.num = num;
+            this.str = str;
+        }
+    }
+
+    public String mostCommonWord(String paragraph, String[] banned) {
+        HashMap<String, Integer> map = new HashMap<>();
+
+        String[] sArr = paragraph.toLowerCase().split("\\W+");
+
+        for (String s : sArr) {
+            if (!s.equals("")) {
+                map.put(s.trim(), map.getOrDefault(s, 0) + 1);
+            }
+        }
+
+        for (String s : banned) {
+            map.put(s, 0);
+        }
+
+        freq maxFreq = new freq(0, "");
+
+        for (String s : sArr) {
+            if (map.containsKey(s) && map.get(s) > maxFreq.num) {
+                maxFreq.num = map.get(s);
+                maxFreq.str = s;
+            }
+        }
+
+        return maxFreq.str;
+    }
+}
