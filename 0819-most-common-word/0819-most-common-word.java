f0fe1e8e286eeb0798701317e6fee6e7643b29ed
HyunsooZo <126321038+HyunsooZo@users.noreply.github.com>
2023-06-09 08:12:08 +0900

    Time: 14 ms (64.77%), Space: 41.9 MB (96.51%) - LeetHub

--- 0819-most-common-word/0819-most-common-word.java
+++ 0819-most-common-word/0819-most-common-word.java
@@ -1,25 +1,41 @@
+
 import java.util.*;
 
 class Solution {
+    private class freq {
+        int num = 0;
+        String str;
+
+        freq(int num, String str) {
+            this.num = num;
+            this.str = str;
+        }
+    }
+
     public String mostCommonWord(String paragraph, String[] banned) {
         HashMap<String, Integer> map = new HashMap<>();
 
         String[] sArr = paragraph.toLowerCase().split("\\W+");
 
-        for (String s : sArr) map.put(s.trim(), map.getOrDefault(s, 0) + 1);
-
-        for (String s : banned) map.put(s, 0);
-
-        int freqCnt=0;
-        String freqStr="";
-
         for (String s : sArr) {
-            if (map.containsKey(s) && map.get(s) > freqCnt) {
-                freqCnt = map.get(s);
-                freqStr = s;
+            if (!s.equals("")) {
+                map.put(s.trim(), map.getOrDefault(s, 0) + 1);
             }
         }
 
-        return freqStr;
+        for (String s : banned) {
+            map.put(s, 0);
+        }
+
+        freq maxFreq = new freq(0, "");
+
+        for (String s : sArr) {
+            if (map.containsKey(s) && map.get(s) > maxFreq.num) {
+                maxFreq.num = map.get(s);
+                maxFreq.str = s;
+            }
+        }
+
+        return maxFreq.str;
     }
 }
