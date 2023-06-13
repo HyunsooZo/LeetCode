fa0c58b737c054b8931b06d0ae4e2d887181fbcf
HyunsooZo <126321038+HyunsooZo@users.noreply.github.com>
2023-06-14 04:42:36 +0900

    Time: 4 ms (85.30%), Space: 57.1 MB (64.51%) - LeetHub

--- 0234-palindrome-linked-list/0234-palindrome-linked-list.java
+++ 0234-palindrome-linked-list/0234-palindrome-linked-list.java
@@ -1,7 +1,6 @@
 class Solution {
     public boolean isPalindrome(ListNode head) {
-        if (head == null || head.next == null)
-            return true;
+        if (head == null || head.next == null) return true;
 
         ListNode fst = head , sec = head;
 
@@ -13,24 +12,21 @@ class Solution {
         ListNode second = reverse(fst) , first = head;
 
         while (second != null) {
-            if (first.val != second.val)
-                return false;
+            if (first.val != second.val) return false;
             first = first.next;
             second = second.next;
         }
-
         return true;
     }
-
+    
     public ListNode reverse(ListNode head) {
-        ListNode prev = null;
-        ListNode curr = head;
+        ListNode prev = null , cur = head;
 
-        while (curr != null) {
-            ListNode next = curr.next;
-            curr.next = prev;
-            prev = curr;
-            curr = next;
+        while (cur != null) {
+            ListNode next = cur.next;
+            cur.next = prev;
+            prev = cur;
+            cur = next;
         }
 
         return prev;
