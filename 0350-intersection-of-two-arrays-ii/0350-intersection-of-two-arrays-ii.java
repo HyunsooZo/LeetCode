c2cce819652acc6d006374b0c816dacd00ec15c9
HyunsooZo <126321038+HyunsooZo@users.noreply.github.com>
2023-06-15 19:36:39 +0900

    Time: 5 ms (21.36%), Space: 42.1 MB (97.81%) - LeetHub

--- 0350-intersection-of-two-arrays-ii/0350-intersection-of-two-arrays-ii.java
+++ 0350-intersection-of-two-arrays-ii/0350-intersection-of-two-arrays-ii.java
@@ -5,8 +5,8 @@ class Solution {
         Arrays.sort(nums1);
         Arrays.sort(nums2);
         
-        int i = 0 , j = 0;
-        while(i<nums1.length && j<nums2.length){
+        int i = 0 , j = 0,len1 = nums1.length , len2 = nums2.length ;
+        while(i<len1 && j<len2){
             if(nums1[i]==nums2[j]){
                 list.add(nums1[i]);
                 i++;
