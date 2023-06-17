45dbba7ec64c62a0a56704931ff52b209b21d58c
HyunsooZo <126321038+HyunsooZo@users.noreply.github.com>
2023-06-18 06:11:10 +0900

    Time: 1 ms (71.38%), Space: 40.7 MB (42.61%) - LeetHub

--- 2103-rings-and-rods/2103-rings-and-rods.java
+++ 2103-rings-and-rods/2103-rings-and-rods.java
@@ -1,15 +1,14 @@
 class Solution {
     public int countPoints(String rings) {
         int[][] answer = new int[10][3];
-        
-        for(int i = 1 ; i < rings.length() ;i++){
-            char c = rings.charAt(i);
-            char b = rings.charAt(i-1);
-            if(Character.isDigit(c)){
-                int cc = Character.getNumericValue(c);
-                if(b=='R') answer[cc][0]++;
-                else if(b=='G') answer[cc][1]++;
-                else if(b=='B') answer[cc][2]++;
+        char[] ringsArr = rings.toCharArray();
+        for(int i = 1 ; i < ringsArr.length ;i++){
+            char cur = ringsArr[i] , prev = ringsArr[i-1];
+            if(Character.isDigit(cur)){
+                int idx = Character.getNumericValue(cur);
+                if(prev=='R') answer[idx][0]++;
+                else if(prev=='G') answer[idx][1]++;
+                else if(prev=='B') answer[idx][2]++;
             }
         }
         int result = 0;
