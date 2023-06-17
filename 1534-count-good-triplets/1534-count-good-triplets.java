df9e05a65189a9ce37bb4beac672ac81345a9d8c
HyunsooZo <126321038+HyunsooZo@users.noreply.github.com>
2023-06-18 06:38:24 +0900

    Time: 13 ms (94.84%), Space: 40.5 MB (49.00%) - LeetHub

--- 1534-count-good-triplets/1534-count-good-triplets.java
+++ 1534-count-good-triplets/1534-count-good-triplets.java
@@ -7,14 +7,14 @@ class Solution {
             for (int j = i + 1; j < n - 1; j++) {
                 if (Math.abs(arr[i] - arr[j]) <= a) {
                     for (int k = j + 1; k < n; k++) {
-                        if (Math.abs(arr[j] - arr[k]) <= b && Math.abs(arr[i] - arr[k]) <= c) {
+                        if (Math.abs(arr[j] - arr[k]) <= b 
+                            && Math.abs(arr[i] - arr[k]) <= c) {
                             count++;
                         }
                     }
                 }
             }
         }
-
         return count;
     }
 }
