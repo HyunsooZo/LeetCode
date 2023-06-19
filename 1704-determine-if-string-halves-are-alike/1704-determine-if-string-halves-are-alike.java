c3589ed251f6b46b108862f20ef1db18850bd9e1
HyunsooZo <126321038+HyunsooZo@users.noreply.github.com>
2023-06-19 23:17:37 +0900

    Time: 2 ms (97.32%), Space: 41.3 MB (19.50%) - LeetHub

--- 1704-determine-if-string-halves-are-alike/1704-determine-if-string-halves-are-alike.java
+++ 1704-determine-if-string-halves-are-alike/1704-determine-if-string-halves-are-alike.java
@@ -3,25 +3,14 @@ class Solution {
         int half = s.length()/2 , aCnt = 0 , bCnt = 0;
         char[] a = s.substring(0,half).toCharArray();
         char[] b = s.substring(half,s.length()).toCharArray();
+        char[] vowls= new char[]{ 'a' ,'e','i','o' ,'u' ,'A','E','I','O','U'};
     
         for(int i = 0 ; i < a.length ; i++){
-            if (a[i] == 'a' || a[i] == 'e' || 
-                a[i] == 'i' || a[i] == 'o' || 
-                a[i] == 'u' || a[i] == 'A' || 
-                a[i] == 'E' || a[i] == 'I' || 
-                a[i] == 'O' || a[i] == 'U') {
-                aCnt++;
-            }
-            if (b[i] == 'a' || b[i] == 'e' || 
-                b[i] == 'i' || b[i] == 'o' || 
-                b[i] == 'u' || b[i] == 'A' || 
-                b[i] == 'E' || b[i] == 'I' || 
-                b[i] == 'O' || b[i] == 'U') {
-                bCnt++;
-            }
-            
+            for(int j = 0 ; j< 10 ; j++){
+                if(a[i]==vowls[j]) aCnt++;
+                if(b[i]==vowls[j]) bCnt++;
+            }   
         }
-
         return aCnt==bCnt;
     }
 }
