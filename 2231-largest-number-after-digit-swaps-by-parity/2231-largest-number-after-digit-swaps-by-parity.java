2ba02dfb20e6f1147d6fee5f38c9c1abba6aedc9
HyunsooZo <126321038+HyunsooZo@users.noreply.github.com>
2023-06-20 01:01:00 +0900

    Time: 1 ms (95.41%), Space: 39.9 MB (36.04%) - LeetHub

--- 2231-largest-number-after-digit-swaps-by-parity/2231-largest-number-after-digit-swaps-by-parity.java
+++ 2231-largest-number-after-digit-swaps-by-parity/2231-largest-number-after-digit-swaps-by-parity.java
@@ -1,10 +1,9 @@
 class Solution {
     public int largestInteger(int num) {
-        String str = Integer.toString(num);
-        char[] charArr = str.toCharArray();
-        int n = charArr.length;
-        for(int i=0;i<n-1;i++){
-            for(int j=i+1;j<n;j++){
+        char[] charArr = Integer.toString(num).toCharArray();
+        int len = charArr.length;
+        for(int i=0;i<len-1;i++){
+            for(int j=i+1;j<len;j++){
                 if(charArr[i]<charArr[j] && charArr[i]%2==0 && charArr[j]%2==0)
                      swap(charArr,i,j);
                 else if(charArr[i]<charArr[j] && charArr[i]%2!=0 && charArr[j]%2!=0)
