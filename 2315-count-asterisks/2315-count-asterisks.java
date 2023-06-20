6781bb2260c6b30d7c516b221a72303be3326fd4
HyunsooZo <126321038+HyunsooZo@users.noreply.github.com>
2023-06-20 14:57:57 +0900

    Time: 2 ms (75.44%), Space: 40.6 MB (88.07%) - LeetHub

--- 2315-count-asterisks/2315-count-asterisks.java
+++ 2315-count-asterisks/2315-count-asterisks.java
@@ -2,17 +2,11 @@ class Solution {
     public int countAsterisks(String s) {
         int answer = 0;
         boolean isItInPair = false;
-        
         for (int i = 0; i < s.length(); i++) {
-            char c = s.charAt(i);
-            
-            if (c == '|') {
-                isItInPair = !isItInPair;
-            } else if (!isItInPair && c == '*') {
-                answer++;
-            }
+            char c = s.charAt(i);   
+            if (c == '|') isItInPair = !isItInPair;
+            else if (!isItInPair && c == '*') answer++;  
         }
-        
         return answer;        
     }
 }
