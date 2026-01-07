40e8abba0b9120351716202edd1175c560690ce8
HyunsooZo <bzhs1992@icloud.com>
2026-01-07 13:18:43 +0900

    Time: 9 ms (6.95%), Space: 46 MB (54.42%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.java
+++ 0009-palindrome-number/0009-palindrome-number.java
@@ -1,17 +1,7 @@
 class Solution {
     public boolean isPalindrome(int x) {
-        if(x<0 || x%10==0 && x!=0){
-            return false;   
-        }else if(x<10){
-            return true;
-        }
-        StringBuilder comparison = new StringBuilder();
-        comparison.append(x);
-        String temp = comparison.toString();
-        if(temp.equals(comparison.reverse().toString())){
-            return true;
-        }else{
-            return false;
-        }
+        String string = String.valueOf(x);
+        String reversed = new StringBuilder(string).reverse().toString();
+        return string.equals(reversed);
     }
 }
