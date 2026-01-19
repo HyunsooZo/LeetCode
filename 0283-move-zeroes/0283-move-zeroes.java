94cf345e70ec741bb244837cdd558e5542f0b55b
HyunsooZo <bzhs1992@icloud.com>
2026-01-19 11:44:03 +0900

    Time: 2 ms (89.53%), Space: 47.7 MB (64.59%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.java
+++ 0283-move-zeroes/0283-move-zeroes.java
@@ -1,7 +1,6 @@
 class Solution {
     public void moveZeroes(int[] nums) {
         int previous = 0;
-        
         for (int next = 0; next < nums.length; next++) {
             if (nums[next] != 0) {
                 int temp = nums[previous];
